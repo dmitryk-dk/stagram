2e5b94a7b17af2a8b6c1c672a311f0477d942354
dmitryk-dk <kozlovdmitriyy@gmail.com>
2017-12-24 20:31:19 +0200

    cleanup

--- src/mutators/posts.js
+++ src/mutators/posts.js
@@ -3,7 +3,6 @@ import getStore from '../store/store';
 import {postsSuccess} from "../actions/posts";
 
 mutator(postsSuccess, (posts) => {
-    console.log("posts ->>", posts)
     const store = getStore();
     store.posts = posts;
 });
