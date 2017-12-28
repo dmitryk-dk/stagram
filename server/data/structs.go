0bed318e3cdfb0d5e9a7ed8dd1c1cea3229f3fca
dmitryk-dk <kozlovdmitriyy@gmail.com>
2017-12-28 10:45:00 +0200

    add structs

---
+++ server/data/structs.go
@@ -0,0 +1,40 @@
+package data
+
+type User struct {
+	UserName string `json:"login"`
+	Password string `json:"password"`
+	Nickname string `json:"nickName"`
+}
+
+type Credentials struct {
+	UserName string `json:"login"`
+	Password string `json:"password"`
+}
+
+type PostsData struct {
+	Posts    interface{} `json:"posts"`
+}
+
+type CommentsData struct {
+	Comments interface{} `json:"comments"`
+}
+
+type Endpoints struct {
+	Login    string `json:"login"`
+	Posts 	 string `json:"posts"`
+	Comments string `json:"comments"`
+	Signup 	 string `json:"signup"`
+	Logout 	 string `json:"logout"`
+}
+
+type Authorization struct {
+	IsAuthed   bool `json:"isAuthed"`
+}
+
+type InitData struct {
+	Endpoints `json:"endpoints"`
+	Authorization
+}
+
+var DbUsers = map[string]User{}
+var DbSessions = map[string]string{}
